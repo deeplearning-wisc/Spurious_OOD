e7699a7401b9304d03316f9143784455bcba5eb5
Alvin_Ming <115010203@link.cuhk.edu.cn>
2021-06-03 21:15:49 -0500

    Create common.py

---
+++ utils/common.py
@@ -0,0 +1,33 @@
+class UnNormalize(object):
+    def __init__(self, mean, std):
+        self.mean = mean
+        self.std = std
+
+    def __call__(self, tensor):
+        """
+        Args:
+            tensor (Tensor): Tensor image of size (C, H, W) to be normalized.
+        Returns:
+            Tensor: Normalized image.
+        """
+        for t, m, s in zip(tensor, self.mean, self.std):
+            t.mul_(s).add_(m)
+        return tensor
+
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += self.val * n
+        self.count += n
+        self.avg = self.sum / self.count
